7511c0d816bcb2aeb068a631cbb492588c3476e2
JosimarProjects <jo030191@gmail.com>
2024-01-27 17:28:53 -0300

    add habilidades e status via chamada com metodo fetch

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Habilidades from "@/views/Habilidades.vue";
+import Sobre from "@/views/Sobre.vue";
+import Status from "@/views/Status.vue";
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {path: '/habilidades', component: Habilidades, name: 'Habilidades'},
+      {path: '/sobre', component: Sobre, name: 'Sobre'},
+      {path: '/status', component: Status, name: 'Status'}
+
+    ]
   }
 ]
 
